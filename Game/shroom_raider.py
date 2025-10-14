a239646b62f0d8b29f24c7cd53e711fa343f5c66
SyedBarkiaNHashemiNik <syedbarkia13@gmail.com>
2025-10-14 20:43:11 +0800

    Fixed the formatting, added two spaces between each function

--- Game/shroom_raider.py
+++ Game/shroom_raider.py
@@ -1,13 +1,17 @@
-# testing testing
 class Grid:
     def __init__(self, rows: int, columns: int):
         self.rows: int = rows
         self.columns: int = columns
 
+
 class Tile:
     def __init__(self, tile_type: str):
         self.tile_type: str = tile_type
 
+
 class Laro:
     def __init__(self, powerup: str = None):
         self.powerup = powerup
+
+
+
